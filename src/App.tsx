29a46de5e445826955c8778b1997e9c6746c80e0
Lem <lemuel.dulfo@gmail.com>
2020-08-09 01:15:30 -0600

    ESLint for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">Navbar</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarColor01">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Features</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Pricing</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">About</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle dropdown show" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">Dropdown</a>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="/">Action</a>
+              <a className="dropdown-item" href="/">Another action</a>
+              <a className="dropdown-item" href="/">Something else here</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="/">Separated link</a>
+            </div>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input className="form-control mr-sm-2" type="text" placeholder="Search" />
+          <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
